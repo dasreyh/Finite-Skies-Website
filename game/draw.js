509e8cbb77a279c0240d14543e772fa68bc10339
Seabass247 <sebjfk@gmail.com>
2017-04-11 20:33:55 -0400

    BAll bounces now

--- game/draw.js
+++ game/draw.js
@@ -2,20 +2,16 @@ var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 20;
 var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+var y = canvas.height-300;
+var gravity = .098;
+var tick = 0;
+var dx = 1;
+var dy = 1;
 
 function drawBall() {
 	"use strict";
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	
-	//Color Gen
-	//  rgbVar = Random number gen           255max 1min
-	var rcolor = Math.floor((Math.random() * 150) + 50);
-	var gcolor = Math.floor((Math.random() * 250) + 50);
-	var bcolor = Math.floor((Math.random() * 250) + 150);
 	
     ctx.fillStyle = "rgba(" + 0 + "," + 150 + "," + 250 + ", 1)";
     ctx.fill();
@@ -24,17 +20,20 @@ function drawBall() {
 function draw() {
 	"use strict";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-
+	drawBall();
+	tick++;
+    dy = dy + (gravity);
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    
-    x += dx;
-    y += dy;
+
+	
+    x += dx;	
+	y += dy;
+
 }
 
 setInterval(draw, 10);
