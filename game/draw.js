cd8cd98085921611051cf0d67453efec08fa9bb3
Brendan Weirtz <brendanweirtz@gmail.com>
2017-04-12 00:39:04 -0400

    Title Screen & Code Cleanup

--- game/draw.js
+++ game/draw.js
@@ -3,9 +3,9 @@ var ctx = canvas.getContext("2d");
 var ballRadius = 20;
 var x = canvas.width/2;
 var y = canvas.height-300;
-var gravity = 0.350; //higher g = stronger force of gravity
-var damping = 0.001; //higher damp = less bouncy
-var friction = 0.0250; //higher f = more ground friction
+var gravity = 1.350; //higher g = stronger force of gravity
+var damping = 0.050; //higher damp = less bouncy
+var friction = 0.0550; //higher f = more ground friction
 var tick = 0;
 var dx = 1;
 var dy = 1;
@@ -13,11 +13,28 @@ var bounceCount = 0;
 var rightPressed = false;
 var leftPressed = false;
 
+ function menuTitle() {
+  ctx.fillStyle = '#f3f3f3';
+  ctx.font = '80px BigNoodleTitling-Oblique,big_noodle_titling_oblique,sans-serif';
+  ctx.textBaseline = 'middle';
+  ctx.fillText('SOLSTICE', 50, 80);
+  }
+
+ function menuList() {
+  ctx.fillStyle = '#f3f3f3';
+  ctx.font = '50px BigNoodleTitling-Oblique,big_noodle_titling_oblique,sans-serif';
+  ctx.textBaseline = 'middle';
+  ctx.fillText('Play', 50, 200);
+  ctx.fillText('Help', 50, 260);
+  ctx.fillText('Settings', 50, 320);
+  }
+
 function drawBall() {
 	"use strict";
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "rgba(" + 180 + "," + 30 + "," + 100 + ", 1)";
+	//               ONLY USE " " in an RGBA color if you are using a var inside of it. Int's dont need " "
+    ctx.fillStyle = "rgba(255,255,255,0.6)";
     ctx.fill();
     ctx.closePath();
 }
@@ -26,7 +43,8 @@ function drawRect() {
 	"use strict";
     ctx.beginPath();
 	ctx.rect(canvas.width/2-20,0,40,canvas.height);	
-    ctx.fillStyle = "rgba(" + 100 + "," + 30 + "," + 180 + ", 1)";
+	//               ONLY USE " " in an RGBA color if you are using a var inside of it. Int's dont need " "
+    ctx.fillStyle = "rgba(100,30,180,1)";
     ctx.fill();
     ctx.closePath();
 }
@@ -35,14 +53,17 @@ function draw() {
 	"use strict";
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawRect();
-
+	menuTitle(); // you can return false to this to make it go away!
+	menuList(); // you can return false to this to make it go away!
 	drawBall();
 	tick++;
     dy = dy / (bounceCount+1);
 	dy = dy + (gravity);
+	
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
+	
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
 		bounceCount += damping;
@@ -53,9 +74,11 @@ function draw() {
 		dy = -2;
 		bounceCount = 0;
 	}
+	
 	if (rightPressed) {
 		dx = dx + gravity;
 	}
+	
 	if (leftPressed) {
 		dx = dx - gravity;
 	}
--- js/glados.js
+++ js/glados.js
@@ -1,7 +1,5 @@
 document.body.onkeyup = function(e){
-	
 	var gladosSound = new Audio('audio/other/glados1.mp3');
-	
     if(e.keyCode === 189){
         document.getElementById('glados').style.display = 'block';
 		gladosSound.play();
