302d3d906931e592d8f748ab7a5569367da7569a
Brendan Weirtz <brendanweirtz@gmail.com>
2017-04-21 21:06:15 -0400

    Link fixes

--- js/glados.js
+++ js/glados.js
@@ -1,5 +1,5 @@
 document.body.onkeyup = function(glad){
-	var gladosSound = new Audio('../audio/other/glados1.mp3');
+	var gladosSound = new Audio('audio/other/glados1.mp3');
 	gladosSound.volume=0.05;
     if(glad.keyCode === 189){
         document.getElementById('glados').style.display = 'block';
