d3ae95d0b3fbc510225b417e410101fdd0bf1c8c
Brendan Weirtz <brendanweirtz@gmail.com>
2017-04-22 03:15:19 -0400

    hero updates

--- js/navScroll.js
+++ js/navScroll.js
@@ -8,32 +8,17 @@ var vscroll = document.body.scrollTop;
 		document.getElementById('logo-image').style.opacity = "1";
 	}
 	
-	
-	if (vscroll > 100) {
+	if (vscroll > 95) {
+		document.getElementById('navbar').style.top = "0";
 		document.getElementById('navbar').style.position = "fixed";
 		document.getElementById('navbar-logo').style.opacity = "1";
-		document.getElementById('navbar').style.background = "white";
-		document.getElementById('navbar').style.boxShadow = "0px 4px 10px rgba(0,0,0,0.1)";
-		document.getElementById('navbar').style.top = "0";
-		var elements = document.getElementsByClassName('navlink'), i, len;
-  		  for (i = 0, len = elements.length; i < len; i++) {
-          elements[i].style.color = 'black';
-          }
 	}else{
-		document.getElementById('navbar').style.position = null;
-		document.getElementById('navbar-logo').style.opacity = null;
-		document.getElementById('navbar').style.background = null;
-		document.getElementById('navbar').style.boxShadow = null;
 		document.getElementById('navbar').style.top = null;
-		var elementsUndo = document.getElementsByClassName('navlink'), j, ben;
-  		  for (j = 0, ben = elementsUndo.length; j < ben; j++) {
-          elementsUndo[j].style.color = null;
-          }
+		document.getElementById('navbar').style.position = null;
+		document.getElementById('navbar-logo').style.opacity = "0";
 	}
-	
-	
 }
-window.setInterval(getNavPosition, 100);
+window.setInterval(getNavPosition, 1);
 // CHECK PAGE position!!!
 // HTML 	<button onClick="Getpostion()"> Get position</button>
 /*
