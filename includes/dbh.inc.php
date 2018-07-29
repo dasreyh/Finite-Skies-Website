b8464a32374d6392f3c2c2ba63f18996e3777371
Brendan Weirtz <brendanweirtz@gmail.com>
2018-07-29 10:45:31 -0400

    temp site holder

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername ="localhost"; //find server name
-$dbUsername ="root";
-$dbPassword =""; //create password
-$dbName ="finite_skies";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
--- js/glados.js
+++
@@ -1,8 +0,0 @@
-document.body.onkeyup = function(glad){
-	var gladosSound = new Audio('audio/other/glados1.mp3');
-	gladosSound.volume=0.05;
-    if(glad.keyCode === 189){
-        document.getElementById('glados').style.display = 'block';
-		gladosSound.play();
-    }
-}
